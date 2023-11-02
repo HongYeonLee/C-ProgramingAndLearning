d7199099dbb73beaf190ec16fedd6937af7d5cd2
HongYeonLee <125536259+HongYeonLee@users.noreply.github.com>
2023-11-02 12:02:18 +0900

    Delete Lab5 directory

--- Lab5/실습1특정구간내의짝수출력.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main()
-{
-	int i;
-	for (i = 30; i <= 60; i += 2)
-		printf("%d ", i);
-	return 0;
-}
--- Lab5/참고3문자의출력.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-	char ch = 'a';
-	printf("%c %c %c", ch, ch+1,ch+2);
-
-	return 0;
-}
